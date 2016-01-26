91cb4b5eb00c4f216739b6e88b211d6d2adf23ed
schlyana <schlyter.f@gmail.com>
2016-01-26 15:09:14 +0100

    Step4 Done Step5 Started

--- Lab1/client.js
+++ Lab1/client.js
@@ -10,11 +10,27 @@ window.onload = function(){
 
 }
 
+function signIn(email, password){
+  alert(email)
+  alert(password)
+  result = serverstub.signIn(email,password)
+
+  if (result.success){
+    alert(result.message)
+    token = result.data
+    return submitFlag=true;
+  }
+  else {
+    alert(result.message)
+    return submitFlag=false;
+  }
+}
+
 function passwordCheck(pw, reppw){
   var passwordLength = 10;
 
   if (pw != reppw){
-    alert("Password mismatch")
+    alert("Password mismatch!")
     return submitFlag=false;
   }
   else if (pw.length < passwordLength) {
@@ -22,7 +38,27 @@ function passwordCheck(pw, reppw){
     return submitFlag=false;
   }
   else {
-    alert("Password accepted")
-    return submitFlag=true;
+    var data = {
+      'email': document.signup.email.value,
+      'password': document.signup.password.value,
+      'firstname': document.signup.firstname.value,
+      'familyname': document.signup.lastname.value,
+      'gender': document.signup.gender.value, /*<---- FEL*/
+      'city': document.signup.city.value,
+      'country': document.signup.country.value
+    };
+
+    result = serverstub.signUp(data)
+    if (result.success){
+      alert(result.message)
+      return submitFlag=true;
+    }
+    else if (!result.success) {
+      alert(result.message)
+      return submitFlag=false;
+    }
+    else {
+      alert("Something went wrong!")
+    }
   }
 }
