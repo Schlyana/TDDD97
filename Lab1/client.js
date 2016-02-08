36099a5751db2d2a407de98af3018df6b0f36811
Schlyana <schlyter.f@gmail.com>
2016-02-08 19:44:31 +0100

    Part 8 left

--- Lab1/client.js
+++ Lab1/client.js
@@ -1,15 +1,15 @@
 displayView = function(){
-
   if (localStorage.getItem("Token") != null){
     view = document.getElementById("profileview").innerHTML;
     viewer = document.getElementById("viewer");
+    viewer.innerHTML = view;
+    showHome();
   }
   else{
     view = document.getElementById("welcomeview").innerHTML;
     viewer = document.getElementById("viewer");
+    viewer.innerHTML = view;
   }
-
-  viewer.innerHTML = view;
 }
 window.onload = function(){
   //code that is executed as the page is loaded.
@@ -22,15 +22,12 @@ function signIn(email, password){
   result = serverstub.signIn(email,password)
 
   if (result.success){
-    alert(result.message)
     token = result.data
     localStorage.setItem("Token", token);
-    return submitFlag=true;
+    localStorage.setItem("email", email);
   }
-  else {
-    alert(result.message)
-    return submitFlag=false;
-  }
+  alert(result.message)
+  return submitFlag=result.success;
 }
 
 function passwordCheck(pw, reppw){
@@ -56,16 +53,86 @@ function passwordCheck(pw, reppw){
     };
 
     result = serverstub.signUp(data)
-    if (result.success){
-      alert(result.message)
-      return submitFlag=true;
-    }
-    else if (!result.success) {
-      alert(result.message)
-      return submitFlag=false;
-    }
-    else {
-      alert("Something went wrong!")
+    alert(result.message)
+    return submitFlag=result.success;
+  }
+}
+
+function showHome(email){
+  document.getElementById('home').style.display = "block";
+  document.getElementById('browse').style.display = "none";
+  document.getElementById('account').style.display = "none";
+
+  if(email == null){
+    result = serverstub.getUserDataByToken(localStorage.getItem("Token"));
+    email = result.data.email;
+    localStorage.setItem("browsemail",email);
+  }
+
+  pi = personalInformation(email);
+  document.getElementById('infofields').innerHTML =  pi;
+  document.getElementById("messages").innerHTML = printMessages(email);
+}
+function showBrowse(){
+  document.getElementById("home").style.display = "none";
+  document.getElementById("browse").style.display = "block";
+  document.getElementById("account").style.display = "none";
+}
+function showAccount(){
+  document.getElementById("home").style.display = "none";
+  document.getElementById("browse").style.display = "none";
+  document.getElementById("account").style.display = "block";
+}
+
+function changePassword(){
+  token = localStorage.getItem("Token");
+  oldpassword = document.changepassword.oldpassword.value;
+  newpassword = document.changepassword.newpassword.value;
+
+  result = serverstub.changePassword(token, oldpassword, newpassword);
+  alert(result.message)
+  return submitFlag=result.success;
+}
+
+function logout(){
+  token = localStorage.getItem("Token");
+  result = serverstub.signOut(token);
+  alert(result.message)
+  if (result.success){
+    localStorage.removeItem("Token");
+    localStorage.removeItem("email");
+  }
+  displayView();
+}
+
+function personalInformation(email){
+  result = serverstub.getUserDataByEmail(localStorage.getItem("Token"),email)
+  result = result.data.email + "<br>" + result.data.firstname + "<br>" + result.data.familyname
+   + "<br>" + result.data.gender + "<br>" + result.data.city + "<br>" + result.data.country;
+  return result;
+}
+
+function printMessages(email){
+  msgs = serverstub.getUserMessagesByEmail(localStorage.getItem("Token"),email);
+  msgLength = msgs.data.length;
+  var messages = "";
+  alert(msgs.message)
+
+  if(msgLength > 0){
+    for(i = 0; i<msgLength; i++){
+      messages = messages + "<br>" + msgs.data[i].writer + ": " + msgs.data[i].content + "<br>";
     }
   }
+  return "<h3>Message wall</h3>" + messages;
+}
+
+function postMessage(){
+  token = localStorage.getItem("Token");
+  email = localStorage.getItem("email");
+  cont = document.getElementById("messagearea").value;
+
+  result = serverstub.postMessage(token,cont,email)
+
+  alert(result.message)
+  return submitFlag = result.success;
 }
